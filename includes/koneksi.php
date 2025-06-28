1a45ee36dbe91493d7b751ea88db5481f9e35c20
PucuBoy <yusuffadilah559@gmail.com>
2025-06-28 12:44:31 +0700

    test docker

--- includes/koneksi.php
+++ includes/koneksi.php
@@ -1,9 +1,18 @@
 <?php
-// koneksi.php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "academic";
+// Deteksi environment (Docker atau Local)
+if (getenv('DOCKER_ENV')) {
+    // Docker
+    $host = "db";
+    $user = "root";
+    $pass = "root";
+    $db   = "academic";
+} else {
+    // XAMPP atau lokal biasa
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db   = "academic";
+}
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 if (!$conn) {
